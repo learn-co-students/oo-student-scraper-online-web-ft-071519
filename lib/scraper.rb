c9bf09a54867d9d5b59b02b4fb5ade4b7a84cd21
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-02 13:45:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,44 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    hash = {}
+    scraped_students = []
     doc = Nokogiri::HTML(open(index_url))
-    binding.pry
+    doc.css("div.student-card").each do|student|
+    
+        hash = {
+      :name => student.css("h4.student-name").text,
+      :location => student.css("p.student-location").text,
+      :profile_url => student.css("a").attr("href").value,
+        }
+        
+        scraped_students << hash
+    end
+    return scraped_students
   end
 
+
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student_contacts [:twitter]= 
+         doc.css("div.social-icon-container")[0].css("a")[0].attr("href") if doc.css("div.social-icon-container")[0].css("a")[0] != nil
+
+    student_contacts[:linkedin] = 
+    doc.css("div.social-icon-container")[0].css("a")[1].attr("href") if doc.css("div.social-icon-container")[0].css("a")[1] != nil
+
+   student_contacts[:github] = 
+       doc.css("div.social-icon-container")[0].css("a")[2].attr("href") if doc.css("div.social-icon-container")[0].css("a")[2] != nil 
+
+   student_contacts[:blog] = 
+       doc.css("div.social-icon-container")[0].css("a")[3].attr("href") if doc.css("div.social-icon-container")[0].css("a")[3] != nil
+      binding.pry
+    student_contacts[:profile_quote] = 
+       doc.css("div.profile-quote").text
+       
+     student_contacts[:bio] = doc.css("div.description-holder p").text
     
+    
+    return student_contacts 
   end
 
 end
@@ -19,3 +51,11 @@ end
 # doc.css("div.student-card")[0].css("h4.student-name").text
 # doc.css("div.student-card")[0].css("p.student-location").text
 # url = doc.css("div.student-card")[0].css("a").attr("href").value
+
+
+# twitter = doc.css("div.social-icon-container")[0].css("a")[0].attr("href")
+#linkedin = doc.css("div.social-icon-container")[0].css("a")[1].attr("href")
+#github = doc.css("div.social-icon-container")[0].css("a")[2].attr("href")
+#blog = doc.css("div.social-icon-container")[0].css("a")[3].attr("href")
+#profile quote = doc.css("div.profile-quote").text
+#bio = doc.css("div.description-holder p").text
