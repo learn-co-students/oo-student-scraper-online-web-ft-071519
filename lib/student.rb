847168534ccf10900ab7a6aaab3a62b3d30db11a
Audrea Cook <audreacwork@gmail.com>
2019-08-04 13:57:12 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student.name = student[name]
+      binding.pry
+    end
   end
 
   def add_student_attributes(attributes_hash)
